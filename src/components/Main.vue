6b4772df8c4d3eb6ac4ff246c90d151751d4e175
Blaser993 <corradodepinto.design@gmail.com>
2023-03-07 17:36:41 +0100

    REFACT fetchMovies

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -7,11 +7,24 @@
                 <button>cerca</button>
             </div>
         </div>
+
+
+        <div>
+            <ul>
+                <li class="film">
+                    <h4>Titolo</h4>
+                    <h5>titolo originale</h5>
+                    <h6>lingua</h6>
+                    <span>voto</span>
+                </li>
+            </ul>
+        </div>
     </main>
 </template>
 
 <script>
 import axios from "axios";
+
 	export default {
         data(){
             return {
@@ -20,18 +33,32 @@ import axios from "axios";
         },
         methods: {
             fetchMovies (){
-                axios.get("https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=io")
-                console.log("fetched movies")
+                axios
+                    .get("https://api.themoviedb.org/3/search/movie",{
+                        params:{
+                            api_key: 'e99307154c6dfb0b4750f6603256716d',
+                            query: "leggenda",
+                            page: 1,
+                        }})
+                    .then((res) => {
+                        console.log(res.data.results)
+                        this.movies = res.data.results
+                    })
+                },
+                
             }
         },
         created (){
             this.fetchMovies()
         }
-	}
 </script>
 
 <style lang="scss" scoped> 
     .input{
         width: 90%;
+    }
+
+    .film{
+        padding: .5rem;
     }
 </style>
