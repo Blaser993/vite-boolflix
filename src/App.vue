a2299cb22349b9e3509d4c86976d38b68ac505c3
Blaser993 <corradodepinto.design@gmail.com>
2023-03-07 15:29:45 +0100

    START

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Header></Header>
+    <Main></Main>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
+import Footer from "./components/Footer.vue";
+
+
+	export default {
+    components: {
+      Header,
+      Main,
+      Footer
+    }
+	}
+</script>
+
+<style lang="scss"> 
+  @use "./style/general.scss";
+
+</style>
