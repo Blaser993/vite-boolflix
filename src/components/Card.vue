4f9b1cb9abd756b434a7c80e6a36f379754ee452
Blaser993 <corradodepinto.design@gmail.com>
2023-03-08 15:14:42 +0100

    FEAT aggiungo bandiera

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,7 +3,9 @@
     v-for="item in store.movies" :key="item.id">
         <h4>{{item.original_title}}</h4>
         <h5>{{item.title}}</h5>
-        <h6>{{item.original_language}}</h6>
+        <h6>{{item.original_language}} 
+            <img :src="addFlag(item.original_language)" alt="">
+        </h6>
         <span>{{(Math.fround(item.vote_average/2).toFixed())}}</span>
     </li>
 </template>
@@ -20,6 +22,23 @@ import store from "../store"
         data(){
             return{
                 store
+            }
+        },
+        methods:{
+            addFlag(original_language){
+                if (original_language === "it"){
+                    this.original_language = store.flags[0]
+
+                } else if (this.original_language === "ja"){
+                    this.original_language = store.flags[1]
+
+                } else if (original_language === "fr"){
+                    this.original_language = store.flags[2]
+
+                } else if (original_language === "ru"){
+                    this.original_language = store.flags[3]
+
+                } else this.original_language = original_language
             }
         }
 	}
--- src/store.js
+++ src/store.js
@@ -4,6 +4,13 @@ const store = reactive({
     movies: [],
     tv: [],
     ricerca:"",
+    flags:[
+        "https://flagcdn.com/16x12/it.png",
+        "https://flagcdn.com/16x12/jp.png",
+        "https://flagcdn.com/16x12/fr.png",
+        "https://flagcdn.com/16x12/ru.png",
+        
+    ],
 
     // configurazione
     API_KEY: 'e99307154c6dfb0b4750f6603256716d',
