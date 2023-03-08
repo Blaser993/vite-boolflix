9ee80a1fd571558d1c60c2225003a8dcaf8325ed
Blaser993 <corradodepinto.design@gmail.com>
2023-03-08 16:29:41 +0100

    FIXED flags

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -4,12 +4,12 @@
         <h4>{{item.original_title}}</h4>
         <h5>{{item.title}}</h5>
         
-        <!-- <img v-if="store.flag.include(item.original_language)"
-         class="iconFlag" :src="addFlag(language)" alt="">
-        <h6 v-else>{{item.original_language}} </h6> -->
 
-        <img class="iconFlag" :src="addFlag(item.original_language)" alt="">
-        
+
+        <img
+        v-if:="addFlag(item.original_language) != null"
+         class="iconFlag" ciao :src="addFlag(item.original_language)" alt="">
+        <h6 v-else>{{ item.original_language }}</h6>
 
         <span>{{(Math.fround(item.vote_average/2).toFixed())}}</span>
     </li>
@@ -32,21 +32,21 @@ import store from "../store"
         methods:{
             addFlag(language){
                 if (language === "it"){
-                    return store.flags[0]
-
-                } else if (language === "ja"){
                     return store.flags[1]
 
-                } else if (language === "fr"){
+                } else if (language === "ja"){
                     return store.flags[2]
 
-                } else if (language === "ru"){
+                } else if (language === "fr"){
                     return store.flags[3]
 
-                }else if (language === "en"){
+                } else if (language === "ru"){
                     return store.flags[4]
 
-                } // else return `<h6>{{${item.original_language}}} </h6>`
+                } else if (language === "en"){
+                    return store.flags[5]
+
+                }  else return null
             }
         }
 	}
